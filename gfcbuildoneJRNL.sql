2a9c7d83dac4f178725225cc07333c6ef32cd89a
davidkurtz <info@go-faster.co.uk>
2022-12-05 17:03:22 +0000

    Additional Partitioning Examples
    for JRNL_LN and PROJ_RESOURCE

---
+++ gfcbuildoneJRNL.sql
@@ -0,0 +1,34 @@
+rem gfcbuildoneJRNL.sql
+rem (c) Go-Faster Consultancy 2001-22
+clear screen
+
+spool gfcbuildoneJRNL
+set echo on termout on serveroutput on
+
+--only necessary if meta data has changed
+
+/**/
+execute gfc_pspart.truncate_tables(p_all=>TRUE);
+set termout off 
+@@partdata
+set termout on 
+/**/
+
+
+spool gfcbuildone
+execute gfc_pspart.truncate_tables;
+execute gfc_pspart.set_defaults(p_debug_level => 0);
+execute gfc_pspart.display_defaults;
+
+begin
+  gfc_pspart.main(p_rectype=>'P', p_part_id=>'JRNLLN');
+end;
+/
+
+
+--pause
+--extract script to file
+@@gfcbuildspool.sql
+
+set head on feedback on termout on pages 50
+
